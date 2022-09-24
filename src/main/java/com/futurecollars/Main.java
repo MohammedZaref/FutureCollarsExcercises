35a4e0d2eb25cb6e831d67422920b711c9cd045f
MohammedZaref <melkady254@gmail.com>
2022-09-24 16:21:50 +0100

    Calling methods from Main class

--- src/main/java/com/futurecollars/Main.java
+++ src/main/java/com/futurecollars/Main.java
@@ -1,7 +1,13 @@
 package com.futurecollars;
-
+import com.futurecollars.lesson7.exc1.ConsoleMessage;
+import com.futurecollars.lesson7.exc1.Message;
+import com.futurecollars.lesson7.exc1.WindowMessage;
 public class Main {
     public static void main(String[] args) {
+        Message windowMsg = new WindowMessage();
+        windowMsg.getMessage();
+        Message consoleMsg = new ConsoleMessage();
+        consoleMsg.getMessage();
 
     }
 }
