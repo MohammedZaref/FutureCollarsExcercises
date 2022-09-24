f53078111f98e9ba9eae5676482eb13403612e22
MohammedZaref <melkady254@gmail.com>
2022-09-24 17:47:42 +0100

    Commented exercise 1

--- src/main/java/com/futurecollars/Main.java
+++ src/main/java/com/futurecollars/Main.java
@@ -4,10 +4,11 @@ import com.futurecollars.lesson7.exc1.Message;
 import com.futurecollars.lesson7.exc1.WindowMessage;
 public class Main {
     public static void main(String[] args) {
-        Message windowMsg = new WindowMessage();
-        windowMsg.getMessage();
-        Message consoleMsg = new ConsoleMessage();
-        consoleMsg.getMessage();
+//        Message windowMsg = new WindowMessage();
+//        windowMsg.getMessage();
+//        Message consoleMsg = new ConsoleMessage();
+//        consoleMsg.getMessage();
+//        EXERCISE 2
 
     }
 }
