edb3ed101b396b16cdc92f83e73cd1f088200c52
MohammedZaref <melkady254@gmail.com>
2022-09-24 16:11:05 +0100

    Created abstract method to get a message

--- src/main/java/com/futurecollars/lesson7/exc1/Message.java
+++ src/main/java/com/futurecollars/lesson7/exc1/Message.java
@@ -1,5 +1,6 @@
 package com.futurecollars.lesson7.exc1;
 
 public abstract class Message {
-
+    //Create abstract method to get a message from console and window
+    public abstract void getMessage();
 }
