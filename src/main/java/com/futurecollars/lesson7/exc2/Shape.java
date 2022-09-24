7d456e5816eec04df0bcdb2e8a2ab3fdb05d9693
MohammedZaref <melkady254@gmail.com>
2022-09-24 17:51:58 +0100

    Defined getArea and getPerimeter methods with 2 double variables as argument (side and base)

---
+++ src/main/java/com/futurecollars/lesson7/exc2/Shape.java
@@ -0,0 +1,7 @@
+package com.futurecollars.lesson7.exc2;
+
+public abstract class Shape {
+    abstract double getArea(double base, double side);
+
+    abstract double getPerimeter(double base, double side);
+}
