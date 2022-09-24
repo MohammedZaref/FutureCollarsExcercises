30ac53774398535bf8c3556e9529f0a54cc11727
MohammedZaref <melkady254@gmail.com>
2022-09-24 16:03:09 +0100

    Created 2 classes extend from Message

---
+++ src/main/java/com/futurecollars/lesson7/exc1/WindowMessage.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lesson7.exc1;
+
+public class WindowMessage extends Message {
+
+    @Override
+    public void getMessage() {
+        System.out.println("This is a Window Message");
+    }
+}
