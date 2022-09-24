c38468632a6fdaa6f4d5ba182ce5cd506780eb8e
MohammedZaref <melkady254@gmail.com>
2022-09-24 23:11:53 +0100

    Refactored Rectangle Class

--- src/main/java/com/futurecollars/lesson7/exc2/Rectangle.java
+++ src/main/java/com/futurecollars/lesson7/exc2/Rectangle.java
@@ -1,15 +1,58 @@
 package com.futurecollars.lesson7.exc2;
 
-public class Rectangle extends Shape{
+public class Rectangle extends Shape {
+    static double width;
+    double length;
 
+    public Rectangle() {
+        super();
+        width = 2;
+        length = 4;
+    }
 
+        public Rectangle(double width, double length) {
+        super();
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String name, double width, double length) {
+        super(name);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
     @Override
-    double getArea(double base, double side) {
-        return base * side;
+    public double getArea() {
+        return width * length;
     }
 
     @Override
-    double getPerimeter(double base, double side) {
-        return (2 * base) + (2 * side);
+    public double getPerimeter() {
+        return (width + length) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "Base = " + width +
+                ",Side = " + length +
+                ", Name = " + name + '\'' +
+                '}';
     }
 }
